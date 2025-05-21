30d0ddcf2cb35dc98e2b6363413f9f13dd5faf47
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-21 15:55:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+string s = "hello";
+Console.WriteLine(ReverseVowels(s));
+string ReverseVowels(string s)
+{
+    Stack<char> stack = new Stack<char>();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < s.Length; i++)
+    {
+        char ch = s[i];
+        if ("aeiouAEIOU".Contains(ch))
+        {
+            stack.Push(ch);
+        }
+    }
+    for (int i = 0; i < s.Length; i++)
+    {
+        char ch = s[i];
+        if ("aeiouAEIOU".Contains(ch))
+        {
+            sb.Append(stack.Pop());
+        }
+        else
+        {
+            sb.Append(ch);
+        }
+    }
+
+    return sb.ToString();
+}
